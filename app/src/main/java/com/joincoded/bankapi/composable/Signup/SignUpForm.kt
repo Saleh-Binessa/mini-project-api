6ae786e89f142fa7d877ff5fa8078ce7956f1ce3
AlKousOthman <alkousothman@gmail.com>
2024-02-01 11:57:59 +0300

    changed input field text color

--- app/src/main/java/com/joincoded/bankapi/composable/Signup/SignUpForm.kt
+++ app/src/main/java/com/joincoded/bankapi/composable/Signup/SignUpForm.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +17,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import com.joincoded.bankapi.viewmodel.BankViewModel
@@ -32,6 +34,7 @@ fun SignUpForm( bankViewModel: BankViewModel ,onSigninClick:() -> Unit
 
     Column {
         OutlinedTextField(
+            colors = OutlinedTextFieldDefaults.colors(Color.White),
             value = username,
             onValueChange = { username = it },
             label = { Text("Username") },
@@ -40,6 +43,7 @@ fun SignUpForm( bankViewModel: BankViewModel ,onSigninClick:() -> Unit
                 .padding(8.dp)
         )
         OutlinedTextField(
+            colors = OutlinedTextFieldDefaults.colors(Color.White),
             value = password,
             onValueChange = { password = it },
             label = { Text("Password") },
@@ -49,6 +53,7 @@ fun SignUpForm( bankViewModel: BankViewModel ,onSigninClick:() -> Unit
                 .padding(8.dp)
         )
         OutlinedTextField(
+            colors = OutlinedTextFieldDefaults.colors(Color.White),
             value = image,
             onValueChange = { image = it },
             label = { Text("Image URL") },
